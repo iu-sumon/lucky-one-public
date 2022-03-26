5ca335b4fccac24ff99defed54e35acc45ce1ed7
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-03-26 21:07:19 +0600

    correction added

--- src/Components/ReactAccordion/ReactAccordion.js
+++ src/Components/ReactAccordion/ReactAccordion.js
@@ -8,8 +8,8 @@ const ReactAccordion = () => {
                 <Accordion.Item eventKey="0">
                     <Accordion.Header>How react works?</Accordion.Header>
                     <Accordion.Body>
-                    React uses a declarative paradigm that makes it easier to reason about User's application and aims to be both efficient and flexible. It designs simple views for each state in your application, and React will efficiently update and render just the right component when your data changes.
-                    React implements a virtual DOM that is basically a DOM tree representation in JavaScript. So when it needs to read or write to the DOM, it will use the virtual representation of it. Then the virtual DOM will try to find the most efficient way to update the browser’s DOM.
+                        React uses a declarative paradigm that makes it easier to reason about User's application and aims to be both efficient and flexible. It designs simple views for each state in your application, and React will efficiently update and render just the right component when your data changes.
+                        React implements a virtual DOM that is basically a DOM tree representation in JavaScript. So when it needs to read or write to the DOM, it will use the virtual representation of it. Then the virtual DOM will try to find the most efficient way to update the browser’s DOM.
 
                     </Accordion.Body>
                 </Accordion.Item>
@@ -19,43 +19,43 @@ const ReactAccordion = () => {
                         <table className="table table-hover table-responsive">
                             <tbody>
                                 <tr>
-                                    
+
                                     <th scope="col" className="text-center">Props</th>
                                     <th scope="col" className="text-center">State</th>
 
                                 </tr>
                                 <tr>
-                                    
+
                                     <td>Props are basically data that flows from one to another component as a parameters. Props can not be modified in a component.</td>
                                     <td>React components has a built-in state object which is private to a component. State can not be accessed from outside of the class. However it can be passed as an argument to another component.</td>
                                 </tr>
                                 <tr>
-                                    
+
                                     <td>Props can be accessed by the child component.</td>
                                     <td>State can not be accessed by the child component.</td>
                                 </tr>
                                 <tr>
-                                   
+
                                     <td>Props are used to communicate between components.</td>
                                     <td>States can be used for rendering dynamic changes with the component.</td>
                                 </tr>
                                 <tr>
-                                    
+
                                     <td>Props are immutable.</td>
                                     <td>State is mutable.</td>
                                 </tr>
                                 <tr>
-                                    
+
                                     <td>Props are read-only.</td>
                                     <td>State changes can be asynchronous.</td>
                                 </tr>
                                 <tr>
-                                     
+
                                     <td>Props make components reusable.</td>
                                     <td>State can not make components reusable.</td>
                                 </tr>
                                 <tr>
-                                     
+
                                     <td>Stateless component can have Props.</td>
                                     <td>Stateless components can not have State.</td>
                                 </tr>
